89b4255b1812db8501cd2c61d0ec3144610317e4
Wassim Triki <wsm@gmail.com>
2023-09-08 10:59:31 +0100

    add editor layout

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,7 @@
 import CustomBtn from "./CustomBtn";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
-export { CustomBtn };
+export { CustomBtn, AIPicker, ColorPicker, FilePicker, Tab };
--- client/src/pages/Editor.jsx
+++ client/src/pages/Editor.jsx
@@ -1,7 +1,61 @@
 import React from "react";
-
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import { download } from "../assets";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+import { EditorTabs, FilterTabs } from "../config/constants";
+import { CustomBtn, Tab } from "../components";
 const Editor = () => {
-  return <div>Editor</div>;
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {!snap.inHomePage && (
+        <>
+          <motion.div
+            key="edit"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomBtn
+              variant="contained"
+              title={"Go Back"}
+              handleClick={() => (state.inHomePage = true)}
+              className={"w-fit px-4 py-2.5 font-bold text-sm"}
+            />
+          </motion.div>
+
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation("up")}
+          >
+            {FilterTabs.map((tab) => (
+              <Tab
+                isFilterTab
+                isActiveTab=""
+                key={tab.name}
+                tab={tab}
+                handleClick={() => {}}
+              />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Editor;
